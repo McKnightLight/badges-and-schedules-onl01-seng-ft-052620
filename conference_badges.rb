92c1bd5b3998772b7d19ea6a5add58cd484afe98
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-28 19:24:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(attendees)
-  attendees.collect.with_index do |attendee, index|
-      #"Hello, Edsger! You'll be assigned to room 1!"
-
+  attendees.collect.with_index(1) do |attendee, index|
+      "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 end
