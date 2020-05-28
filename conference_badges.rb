6fd36b1085b6d7389826a9cae2e14f907632ea41
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-28 19:15:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,11 @@ end
 def batch_badge_creator(attendeesName)
   return batch_badge_creator
 end
+
+Hello, my name is Edsger.",
+"Hello, my name is Ada.",
+"Hello, my name is Charles.",
+"Hello, my name is Alan.",
+"Hello, my name is Grace.",
+"Hello, my name is Linus.",
+                  "Hello, my name is Matz."
